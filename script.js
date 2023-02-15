a93c92bf2e22f8941e442e31c408a0826a436f61
Higor Gabriel <higor448.pessoal@gmail.com>
2023-02-15 16:47:08 -0300

    requisito 3, 4 feitos, validador de login feito

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const btnSubmit = document.getElementById('btn-submit');
+
+const validaForm = () => {
+    if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+        return alert('Olá, Tryber!');
+    }
+    return alert('Email ou senha inválidos.');
+};
+
+btnSubmit.addEventListener('click', validaForm);
