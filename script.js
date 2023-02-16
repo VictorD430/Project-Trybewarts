3db32c79020e9c1e356466eeacd499c6484bcf78
Higor Gabriel <higor448.pessoal@gmail.com>
2023-02-15 22:02:23 -0300

    requisito 18, 19, 20, e algumas correções

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
-const btnSubmit = document.getElementById('btn-submit');
+const btnLogin = document.getElementById('btn-login');
+const btnSubmit = document.getElementById('submit-btn');
+const agree = document.getElementById('agreement');
+const textarea = document.getElementById('textarea');
+const contadorPRO = document.getElementById('counter');
 
-const validaForm = () => {
+const validaLogin = () => {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
     return alert('Olá, Tryber!');
   }
   return alert('Email ou senha inválidos.');
 };
+btnLogin.addEventListener('click', validaLogin);
 
-btnSubmit.addEventListener('click', validaForm);
+const validaForm = () => {
+  if (agree.checked) {
+    btnSubmit.disabled = false;
+  }
+};
+agree.addEventListener('change', validaForm);
+
+textarea.addEventListener('keypress', function(e) {
+  const inputLength = textarea.value.length;
+  const maxChars = 499;
+  let contagem = maxChars - inputLength;
+  contadorPRO.innerHTML = contagem + '/500';
+});
